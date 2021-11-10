7f556194a9fb43efe64cf3632387566aa2e3f875
LofizDev <86564838+LofizDev@users.noreply.github.com>
2021-11-10 15:23:10 +0700

    add action payload Loading

--- client/src/redux/actions/authAction.js
+++ client/src/redux/actions/authAction.js
@@ -7,14 +7,23 @@ export const TYPES = {
 // Login
 export const login = (data) => async (dispatch) => {
     try {
+        // Loading
         dispatch({type: 'NOTIFY', payload: { loading: true }})
+        // Get data from API
         const res = await postDataAPI('login',data )
-        localStorage.setItem("firstLogin",true)
         dispatch({
             type:'AUTH',
             payload: {
                 token:res.data.access_token,
                 user:res.data.user
+            }
+        })
+        localStorage.setItem("firstLogin",true)
+        // Send notify success or error
+        dispatch({
+            type:'NOTIFY',
+            payload: {
+                success:res.data.msg
             }
         })
     } catch (error) {
