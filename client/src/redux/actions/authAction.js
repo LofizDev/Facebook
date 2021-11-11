8d259370e7f76dc1b21f692f37b04449bd6e7982
LofizDev <86564838+LofizDev@users.noreply.github.com>
2021-11-11 12:52:43 +0700

    get token in localStorage and call api to auto login

--- client/src/redux/actions/authAction.js
+++ client/src/redux/actions/authAction.js
@@ -4,34 +4,61 @@ export const TYPES = {
     AUTH: 'AUTH'
 }
 
-// Login
+// Login, Call to API and response then save Token when Login
 export const login = (data) => async (dispatch) => {
     try {
         // Loading
-        dispatch({type: 'NOTIFY', payload: { loading: true }})
+        dispatch({ type: 'NOTIFY', payload: { loading: true } })
         // Get data from API
-        const res = await postDataAPI('login',data )
+        const res = await postDataAPI('login', data)
         dispatch({
-            type:'AUTH',
+            type: 'AUTH',
             payload: {
-                token:res.data.access_token,
-                user:res.data.user
+                token: res.data.access_token,
+                user: res.data.user
             }
         })
-        localStorage.setItem("firstLogin",true)
+        // Save token in localStorage
+        localStorage.setItem("firstLogin", true)
         // Send notify success or error
         dispatch({
-            type:'NOTIFY',
+            type: 'NOTIFY',
             payload: {
-                success:res.data.msg
+                success: res.data.msg
             }
         })
     } catch (error) {
         dispatch({
-            type:'NOTIFY',
+            type: 'NOTIFY',
             payload: {
-                error:error.response.data.msg
+                error: error.response.data.msg
             }
         })
+    }
+}
+
+// Check if token exists locaStorage then call API ( Automatically Login )
+export const refreshToken = () => async (dispatch) => {
+    const firstLogin = localStorage.getItem("firstLogin")
+    if (firstLogin) {
+        dispatch({ type: 'NOTIFY', payload: { loading: true } })
+        try {
+            const res = await postDataAPI('refresh_token')
+            dispatch({
+                type: 'AUTH',
+                payload: {
+                    token: res.data.access_token,
+                    user: res.data.user
+                }
+            })
+            dispatch({ type: 'NOTIFY', payload: {} })
+        } catch (error) {
+            dispatch({
+                type: 'NOTIFY',
+                payload: {
+                    error: error.response.data.msg
+                }
+            })
+        }
     }
 }
