98637c5b8d7ef4e5279b0bcd18f46a143bcac14a
LofizDev <86564838+LofizDev@users.noreply.github.com>
2021-11-13 10:56:30 +0700

    udpate history route

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useStyles } from './style'
 import TextField from '@material-ui/core/TextField';
 import clsx from 'clsx';
@@ -6,21 +6,24 @@ import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { register } from '../../redux/actions/authAction'
-import { useSelector, useDispatch} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 function Register() {
     const classes = useStyles();
     const dispatch = useDispatch()
-    const [userData,setUserData] = useState({ 
-        fullname:'',username:'',email:'',password:'',cfpassword:'',gender:'male'
+    const [userData, setUserData] = useState({
+        fullname: '', username: '', email: '', password: '', cfpassword: '', gender: 'male'
     })
+
+    const { auth } = useSelector(state => state)
+    const history = useHistory()
 
     // Hanlde change input
     const handleChangeInput = (e) => {
         const value = e.target.value
-        setUserData({ ...userData,[e.target.name]:value })
+        setUserData({ ...userData, [e.target.name]: value })
     }
 
     // Handle submit
@@ -28,14 +31,20 @@ function Register() {
         e.preventDefault()
         dispatch(register(userData))
     }
-    
+
+    // Check correct router
+    useEffect(() => {
+        if(auth.token) history.push("/")
+    }, [auth.token, history])
+
+
     return (
         <div className={classes.registerContainer}>
             <form
                 onSubmit={handleSubmit}
                 className={classes.form} noValidate autoComplete="off">
                 <h1>Đăng ký</h1>
-                <span style={{ marginBottom: '10px',color:'#606770' }}>Nhanh chóng và dễ dàng</span>
+                <span style={{ marginBottom: '10px', color: '#606770' }}>Nhanh chóng và dễ dàng</span>
                 <TextField
                     className={clsx(classes.input, classes.fullname)}
                     label="Họ và tên"
@@ -45,7 +54,7 @@ function Register() {
                     variant="outlined" />
                 <TextField
                     className={clsx(classes.input, classes.username)}
-                    label="User Name" 
+                    label="User Name"
                     value={userData.username}
                     name='username'
                     onChange={handleChangeInput}
@@ -73,19 +82,19 @@ function Register() {
                     variant="outlined" />
                 <FormControl component="fieldset">
                     <RadioGroup
-                     style={{flexDirection:'row', justifyContent:'space-between'}} 
-                     value={userData.gender}
-                     onChange={handleChangeInput}>
+                        style={{ flexDirection: 'row', justifyContent: 'space-between' }}
+                        value={userData.gender}
+                        onChange={handleChangeInput}>
                         <FormControlLabel name='gender' value="male" control={<Radio />} label="Nam" />
-                        <FormControlLabel name='gender'value="female" control={<Radio />} label="Nữ" />
-                        <FormControlLabel name='gender'value="other" control={<Radio />} label="Khác" />
+                        <FormControlLabel name='gender' value="female" control={<Radio />} label="Nữ" />
+                        <FormControlLabel name='gender' value="other" control={<Radio />} label="Khác" />
                     </RadioGroup>
                 </FormControl>
                 <button className={classes.create}>Đăng ký</button>
-                <div style={{ display: 'flex'}}>
+                <div style={{ display: 'flex' }}>
                     <span className={classes.recorved}>Bạn đã có tài khoản?</span>
                     <Link to='/login'>
-                      <p style={{ color: 'red',marginLeft:'5px' }}>Đăng nhập</p>
+                        <p style={{ color: 'red', marginLeft: '5px' }}>Đăng nhập</p>
                     </Link>
                 </div>
             </form>
