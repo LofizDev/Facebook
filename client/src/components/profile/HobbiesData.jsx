c627daf8e139f22ae25d5c868660c42dc63a80c4
LofizDev <86564838+LofizDev@users.noreply.github.com>
2021-11-28 22:34:33 +0700

    add Bio UI

---
+++ client/src/components/profile/HobbiesData.jsx
@@ -0,0 +1,89 @@
+export const HobbiesData = [
+    {
+        hoobie: "⛰️ Leo núi"
+    },
+    {
+        hoobie: "💻 Fix bugs"
+    },
+    {
+        hoobie: "⛵ Đua ghe"
+    },
+    {
+        hoobie: "🐕 Chọc chó"
+    },
+    {
+        hoobie: "🎧 Nghe nhạc"
+    },
+    {
+        hoobie: "🐎 Cưỡi ngựa"
+    },
+    {
+        hoobie: "✈️ Láy máy bay"
+    },
+    {
+        hoobie: "🏀 Bóng chuyền"
+    },
+    {
+        hoobie: "⚽ Bóng đá"
+    },
+    {
+        hoobie: "💤 Ngủ"
+    },
+    {
+        hoobie: "🛣️ Phượt Đà lạt"
+    },
+    {
+        hoobie: "🃏 Kéo xì dách"
+    },
+    {
+        hoobie: "🍕️ Ăn uống"
+    },
+    {
+        hoobie: "🌏 Du lịch"
+    },
+    {
+        hoobie: "📖 Đọc sách"
+    },
+    {
+        hoobie: "🔮 Xem bói"
+    },
+    {
+        hoobie: "🍳 Nấu ăn"
+    },
+    {
+        hoobie: "🎤 Ca hát"
+    },
+    {
+        hoobie: "👥 Gặp gỡ bạn bè"
+    },
+    {
+        hoobie: "📸 Nhiếp ảnh"
+    },
+    {
+        hoobie: "🎱 Bi-a lỗ & bi-a"
+    },
+    {
+        hoobie: "🃏 Ảo thuật bài"
+    },
+    {
+        hoobie: "😆️ Ảnh chế"
+    },
+    {
+        hoobie: "🎭 Hài kịch ứng biến"
+    },
+    {
+        hoobie: "🏉 Bóng bầu dục xe lăng"
+    },
+    {
+        hoobie: "🗝 Sưu tầm đồ cổ"
+    },
+    {
+        hoobie: "🔮 Chiêm tinh học"
+    },
+    {
+        hoobie: "♿ Đua xe lăn"
+    },
+    {
+        hoobie: "🏙️ Đầu tư bất động sản"
+    },
+]
--- client/src/components/profile/TilesFeedAbout.jsx
+++ client/src/components/profile/TilesFeedAbout.jsx
@@ -1,17 +1,85 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useStyles } from './style'
 import { useTranslation } from 'react-i18next'
+import HobbiesBox from './dialogs/HobbiesBox'
+
 function TilesFeedAbout() {
     const classes = useStyles()
     const { t } = useTranslation()
+    const [addBio, setAddBio] = useState(false)
+    const [addHobbies, setAddHobbies] = useState(false)
+    const [countDown, setCountDown] = useState(101)
+    const [allowedToSave, setAllowedToSave] = useState(false)
+    const [updateProfile, setUpdateProfile] = useState({ bio: "" })
+
+    const handleChangeValue = (e) => {
+        const value = e.target.value
+        setUpdateProfile({ ...updateProfile, [e.target.name]: value })
+    }
+
+    // Check Allow To Save
+    useEffect(() => {
+        setCountDown(101 - updateProfile.bio.length)
+        if (updateProfile.bio.length > 0 && updateProfile.bio.length < 102) {
+            setAllowedToSave(true)
+        } else {
+            setAllowedToSave(false)
+        }
+    }, [updateProfile.bio])
+
+
     return (
-        <div className={classes.tilesFeedAbout}>
-            <h2 style={{ fontSize: '21.5px', marginBottom: '5px' }}> {t('gioithieu')} </h2>
-            <span className={classes.label}> {t('themtieusu')} </span>
-            <span className={classes.label}> {t('chinhsuachitiet')} </span>
-            <span className={classes.label}> {t('themsothich')} </span>
-            <span className={classes.label}> {t('chinhsuaphandangchuy')} </span>
-        </div>
+        <>
+            <div className={classes.tilesFeedAbout}>
+                <h2 style={{ fontSize: '21.5px', marginBottom: '10px' }}> {t('gioithieu')} </h2>
+                {/* Add Bio */}
+                {addBio === false && (
+                    <span
+                        onClick={() => setAddBio(true)}
+                        className={classes.label}>
+                        {t('themtieusu')}
+                    </span>
+                )}
+                {addBio && (
+                    <div className={classes.bio}>
+                        <textarea
+                            onChange={handleChangeValue}
+                            name="bio"
+                            className={classes.inputBio}
+                            placeholder='Mô tả về bạn'>
+                        </textarea>
+                        <div className={classes.countDown}>
+                            <span className={classes.textCountDown}>Còn {countDown} ký tự</span>
+                            <div className={classes.btnGroupCountDown}>
+                                <button
+                                    id={allowedToSave ? 'allow-to-save' : 'cant-save'}
+                                    style={{ opacity: allowedToSave ? '1' : '.6' }}
+                                    className={classes.btnSave}>
+                                    Lưu
+                                </button>
+                                <button onClick={() => setAddBio(false)} className={classes.btnCancel}>Hủy</button>
+                            </div>
+                        </div>
+
+                    </div>
+                )}
+
+
+                <span className={classes.label}> {t('chinhsuachitiet')} </span>
+                {/* Add Hobbies */}
+                <span
+                    onClick={() => setAddHobbies(true)}
+                    className={classes.label}>
+                    {t('themsothich')}
+                </span>
+                <span className={classes.label}> {t('chinhsuaphandangchuy')} </span>
+            </div>
+
+            {/* Overlay Add Hobbies */}
+            {addHobbies && (
+                <HobbiesBox setAddHobbies={setAddHobbies} />
+            )}
+        </>
     )
 }
 
