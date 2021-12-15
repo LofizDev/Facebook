4330cf1399f0407ede5682fb103e3db2e9452730
LofizDev <86564838+LofizDev@users.noreply.github.com>
2021-12-15 21:08:28 +0700

    loading update

--- client/src/components/notify/LoadingSearch.jsx
+++ client/src/components/notify/LoadingSearch.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './search.style.css'
+import './loadingSearch.css'
 function LoadingSearch() {
     return (
         <>
---
+++ client/src/components/notify/LoadingUpdate.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './loadingUpdate.css'
+function LoadingUpdate() {
+    return (
+        <div className='loadingUpdate'>
+            <div className="load-4">
+                <div className="ring-1"></div>
+            </div>
+        </div>
+    )
+}
+
+export default LoadingUpdate
