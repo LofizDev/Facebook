d1ec230f4427f744be3b1978122fa189fa179f58
LofizDev <86564838+LofizDev@users.noreply.github.com>
2021-12-17 11:49:26 +0700

    loading Button

---
+++ client/src/components/notify/loadingButton/LoadingButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './loadingButton.css'
+function LoadingButton() {
+    return (
+        <>
+            <div class="loadingg">
+                <div class="loading-dot"></div>
+                <div class="loading-dot"></div>
+                <div class="loading-dot"></div>
+            </div>
+        </>
+
+    )
+}
+
+export default LoadingButton
