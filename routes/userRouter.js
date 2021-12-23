c1e15de60a96544ae525ecde6bbccc8b35a82e41
LofizDev <86564838+LofizDev@users.noreply.github.com>
2021-12-23 14:53:46 +0700

    add router follow, unfollow

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,15 @@
- const router = require('express').Router()
- const userController = require('../controllers/user.controller')
+const router = require('express').Router()
+const auth = require('../middleware/auth')
+const userController = require('../controllers/user.controller')
 
- router.get('/search', userController.searchUser)
+router.get('/search', userController.searchUser)
 
- router.get('/getUser', userController.getUser)
+router.get('/user/:id', userController.getUser)
 
- module.exports = router
+router.patch('/user', auth, userController.updateUserInfo)
+
+router.patch('/user/:id/follow', auth, userController.follow)
+
+router.patch('/user/:id/unfollow', auth, userController.unFollow)
+
+module.exports = router
